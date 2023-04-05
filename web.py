6b6195c94abdf67d4417ba508b8966d8ecc5dcfe
minsu <rkdalstn9788@gmail.com>
2023-04-05 16:40:11 +0900

    [메인페이지] 예약중인 유저의 프로필 표시하도록 구현

--- web.py
+++ web.py
@@ -118,39 +118,28 @@ def generate_qr_code(url):
 
     return send_file(img_io, mimetype='image/png')
 
-# @app.route('/url/<string:item_id>', methods=['POST'])
-# def url(item_id):
-#     # 여기에서 필요한 처리를 수행합니다.
-#     data = {
-#         "content": {
-#             "action": {
-#                 "type": "LINK",
-#                 "value": "https://www.daangn.com/articles/"+item_id
-#             },
-#             "description": "네이버앱에서 웹페이지를 확인하실 수 있습니다.",
-#             "title": "PC 웨일에서 웹페이지가 전송되었습니다."
-#         },
-#         "pushDevice": {
-#             "appId": "APG00012",
-#             "deviceType": "apns",
-#             "duId": "dc5b1455a1af44f20e545236b4d3cb09"
-#         }
-#     }
-#     response_url = "http://apis.naver.com/whale/tube_api/push?msgpad=1680593278015&md=k4kNp8QufgrL%2FOnMC5yRkT6kftk%3D"
-#     response_headers = {
-#         'x-user-agent' : 'TowneersApp/23.2.2/230202 iOS/16.2.0/1953.3 iPhone14,2',
-#         'accept' : '*/*',
-#         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Whale/3.19.166.16 Safari/537.36',
-#         'Accept-Encoding' : 'gzip, deflate',
-#         'accept-language' : 'ko-KR',
-#         'Content-Type' : 'application/json',
-#         'Cookie' : 'NID_AUT=kCCZsVklch86Rbo3dKubNw7+YRaom9lBK6FYYdXltQcmzD1TO9J6mUBD6eHrnEGU;NID_JKL=fW2EpfuB+ei2aWvU4sQyhcdD3R1+WqQGRxBNgECLlk4=;NID_SES=AAABw7EDlSVps+1gSfC/xlq8pbny1Sno99+ZcixNzXC7dor4t7dyTjj2KiQUfAqV3BlxOlhezciESH1wgefwJC4pEGK6KC2EJlfxmV0wyWHGuA2wegC0mylVW8XdY0AUKUYt+f0IoT84pS6q2Ion0yRL2LLLG4S1DHFpa5DICmDLt/JcEdFqkWWg8ie2NcHMwmz+mW96lnxWLz2PvgYSOwOHJNGpw3RRs7KQn5RDWXl16tE+g50PocZUrDabYqugzxlYhA030eDMPQ3paVNxMF5mXaWJlHG+GDzdgTywQ7AT5MYhZirVFHTaqUCGMPJhciGhgr3SjsrdszKAGHtcn1AXRqk3oeUd3SHwFzzT0afAIdnooeZ25ELXMBvKFg0f0g2xia7MyejTCZ/NdIYH1EefRi+3fqDgWiL01y2cBt79ghdzMA0pMOqnBR0HqKQnD6xABEoXNLl8Y3HpRCCx9erkyBZLXEqz93CMGkflRDIcXeVcrA5nVxhVPBPCIZJkCXobT1Yvro69kEWQkdomFJPU0J082DbFk21NYVUEOJYjriRbGrKwek83hZ4qv9jrOtYpOFbfcjw0Q66vnoI3pxT2SsRbJXBjQqjqsLmyIWBc4NaO;'
-#         }
-#     response = requests.post(url=response_url, headers=response_headers, json=data)
-    
-#     res_json = json.loads(response.text)
-#     # 원하는 결과를 JSON 형식으로 반환합니다.
-#     return jsonify(res_json)
+@app.route('/user/<string:user_id>', methods=['POST'])
+def user(user_id):
+    # 여기에서 필요한 처리를 수행합니다.
+    response_url = "https://api.kr.karrotmarket.com/webapp/api/v24/profiles/"+user_id+".json"
+    response_headers = {
+        'x-user-agent' : 'TowneersApp/23.2.2/230202 iOS/16.2.0/1953.3 iPhone14,2',
+        'accept' : '*/*',
+        'x-country-code' : 'KR',
+        'x-karrot-api-content-sha256' : '47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=',
+        'x-karrot-api-signature-v1' : 'c8455d1f9261ec307a93e2e467c9234ba9ad6388285352c316638763b9f627f2',
+        'accept-language' : 'ko-KR',
+        'x-karrot-api-timestamp' : '1680497117015',
+        'accept-encoding' : 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+        'user-agent' : 'Karrot/23.2.2 (com.towneers.www; build:0; iOS 16.2.0) Alamofire/5.6.1',
+        'X-Auth-Token' : '79771223201a41e4fbffde341db55f48',
+        'Cookie' : '_hoian-webapp_session=kYFv3tqPpqth0Pv%2FlWOaHLZK8QHgZhCkzHkazhOl2sbVng%2BGLjDP04G1Ug85ujNwx0YlHczC0i%2BryzC8S2pfUWkxj9t%2B7vEm%2F17HbNhqXxt6wgVQmNxu3u4c8LXS32l7WQ0ScYkdFc%2FeH3cnAuJE8g3UL5BNZPnOSSvau0cCccxsHyVzu0MTN5CEmLq0%2FQYMr2TV9Nh0VUiz%2BYh2ROlMOM7RQwHJ--%2BSohH2xPwOfCWUPq--iXId19Ku%2F8TIRv0s79p0hA%3D%3D'
+        }
+    response = requests.get(url=response_url, headers=response_headers)
+    res_json = json.loads(response.text)
+
+    # 원하는 결과를 JSON 형식으로 반환합니다.
+    return jsonify(res_json)
 
 
 @app.route('/updates/<string:item_id>', methods=['POST'])
